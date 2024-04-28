753df63b75dfcfc67211dcce9a7dd07af665f780
Hans Queja <hansqueja8@gmail.com>
2024-04-28 22:31:56 +0800

    Created a python script for collaborators in the GDSC repository

---
+++ GDSC.py
@@ -0,0 +1,16 @@
+
+def main():
+    
+    addition = 1 + 1
+
+    if addition == 3:
+        print("Welcome to PUP GDSC!")
+    else:
+        print("BSCS 2-3")
+        print("Hans Christian N. Queja")
+        print("Let's Go Software Development Team!")
+
+
+
+if __name__ == "__main__":
+    main()
